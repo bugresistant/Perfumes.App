838802cf5fe3a0daa4448f87dc49de0a9a022592
Ivan Malishevskyi <ivanmalishevskiy0@gmail.com>
2024-05-17 20:09:50 +0200

    A new exception and a new derived from OdLiq classes are created

---
+++ Liquids/AirFreshenerLiquid.cs
@@ -0,0 +1,18 @@
+﻿using Perfumes.App.Utility;
+
+namespace Perfumes.App.Liquids;
+
+internal class AirFreshenerLiquid: OdorousLiquids
+{
+    LiquidType specificLiquidType = LiquidType.AirFreshenerLiquid;
+
+    internal override string NumberOfAppliesToSkin
+    {
+        get
+        {
+            throw new ToxicSubstanceException(
+                "ERROR: Air freshener liquids are toxic and cannot be applied to the skin, be careful!");
+            Console.ResetColor();
+        } 
+    }
+}
---
+++ Utility/ToxicSubstanceException.cs
@@ -0,0 +1,10 @@
+﻿namespace Perfumes.App.Utility;
+
+public class ToxicSubstanceException : Exception
+{
+    public ToxicSubstanceException(string message) : base(message)
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.BackgroundColor = ConsoleColor.Gray;
+    }
+}
